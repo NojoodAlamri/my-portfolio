6ec131a4a7b0729120b9a356d73f76dca350beb5
Nojood <66563207+NojoodAlamri@users.noreply.github.com>
2025-05-14 15:36:56 +0300

    home

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eng. Nojood - Portfolio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.12.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="./style/styles.css">
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark custom-navbar fixed-top">
+    <div class="container">
+        <div class="collapse navbar-collapse justify-content-center">
+
+            <nav class="custom-rounded-navbar">
+                <ul class="nav-list">
+                    <li><a href="#about" class="nav-link active">About</a></li>
+                    <li><a href="#highlights" class="nav-link">Highlights</a></li>
+                    <li><a href="#skills" class="nav-link">Skills</a></li>
+                    <li><a href="#tools" class="nav-link">Tools & Tech</a></li>
+                    <li><a href="#projects" class="nav-link">Projects</a></li>
+                    <!--                    <li><a href="#projects" class="nav-link">certificates</a></li>-->
+                    <li><a href="#contact" class="nav-link">Contact</a></li>
+                    <li>
+                        <a href="#" class="btn btn-gradient" title="Download Resume">
+                            <i class="fa-solid fa-file-arrow-down"></i></a>
+
+                    </li>
+
+                </ul>
+            </nav>
+
+        </div>
+    </div>
+</nav>
